5e7c90895d5cbede5b601eb3fa05e8b96d1b37ae
Krishna Bhatta <kkrishnabhattakb@gmail.com>
2025-06-26 17:46:44 +0545

    Overall website

---
+++ Js/script.js
@@ -0,0 +1,9 @@
+  // java script for add to cart alert message //
+  function myFunction(){
+    alert("Product Sucessfully added to cart!")
+}
+
+const cartbutton=document.querySelector(".cart")
+cartbutton.addEventListener("click",function(){
+    alert("Sign In to open the Cart")
+})
